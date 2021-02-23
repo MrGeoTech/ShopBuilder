a981706448c790b6c4202538aaf72c19e69d70c9
MrGeoTech <themrgeotech@gmail.com>
2021-02-23 17:01:19 -0600

    Finished GUI Item

--- ShopBuilder/src/main/java/me/mrgeotech/gui/items/GUIItem.java
+++ ShopBuilder/src/main/java/me/mrgeotech/gui/items/GUIItem.java
@@ -9,30 +9,32 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import me.mrgeotech.processes.Triggered;
+import net.md_5.bungee.api.ChatColor;
 
 public class GUIItem {
 	
 	private ItemStack item;
 	
-	
+	private Triggered triggered;
 	
 	public GUIItem(Material mat, int amount, String name, List<String> lore, Triggered triggered) {
 		item = new ItemStack(mat, amount);
 		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName(name);
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
 		meta.setLore(lore);
 		item.setItemMeta(meta);
 		
 	}
 	
 	public void setTriggered(Triggered triggered) {
-		triggered.run();
+		this.triggered = triggered;
 	}
 	
 	@EventHandler
 	public void onClick(InventoryClickEvent e) {
 		if (e.getCurrentItem() != item) return;
-		
+		e.getWhoClicked().closeInventory();
+		triggered.run();
 	}
 	
 }
